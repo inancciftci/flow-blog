7394a369c2f1c2603db89050620f79de614484cf
inancciftci <enonch@hotmail.com>
2025-03-04 15:23:09 +0300

    Fix deployment issue with params

--- app/(root)/blog/category/[slug]/page.tsx
+++ app/(root)/blog/category/[slug]/page.tsx
@@ -7,25 +7,28 @@ import React, { useEffect, useState } from "react";
 const Page = ({ params }: { params: { slug: string } }) => {
   const [categoryPosts, setCategoryPosts] = useState<Post[]>([]);
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const fetchCategoryPosts = async () => {
-      const { slug } = await params;
-      const category = await api.categories.getBySlug(slug);
+      const category = await api.categories.getBySlug(params.slug);
       const posts = await api.posts.getByCategory(category.id);
       setCategoryPosts(posts);
       setLoading(false);
     };
     fetchCategoryPosts();
-  }, [params]);
-  if (loading)
+  }, [params.slug]);
+
+  if (loading) {
     return (
       <div className="h-[100vh]">
         <PostLoading />
       </div>
     );
+  }
+
   return (
     <div className="flex flex-col gap-10 my-6">
-      {categoryPosts.map((post: Post) => (
+      {categoryPosts.map((post) => (
         <CategoryPost key={post.id} post={post} />
       ))}
     </div>
