56ba55fd2226b1f580499c1c843596de1b7ef4bc
inancciftci <enonch@hotmail.com>
2025-03-04 18:05:31 +0300

    runtime = "edge to homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import RecentArticles from "@/components/RecentArticles";
 import TrendingTopics from "@/components/TrendingTopics";
 import { api } from "@/lib/api";
+export const runtime = "edge";
 
 export default async function Home() {
   const posts = await api.posts.getAll();
