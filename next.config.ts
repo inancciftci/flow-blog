58c792b3e1483287e3746fe246a2b3860ad0c7ff
inancciftci <enonch@hotmail.com>
2025-04-15 21:26:59 +0300

    typescript ignoreBuildErrors - (testing purposes)

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["wp.alithemes.com", "zmktljhsixtnepoianod.supabase.co"],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
